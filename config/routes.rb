efcfef1438aaa71f7703938ab488fefcc69fe9c0
Nate Allen <neight.allen@gmail.com>
2017-01-04 09:54:38 -0700

    Added users model/controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+
   root to: "links#index"
 
   resources :links, only: [:index]
+  resources :users, only: [:new, :create]
 
   namespace :api do
     namespace :v1 do
