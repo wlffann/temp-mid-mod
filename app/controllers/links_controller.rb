2911a8895d1c2c8bc90408428413d48cf73a6891
Nate Allen <neight.allen@gmail.com>
2017-01-04 11:23:12 -0700

    Those reads are hot

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,3 +1,5 @@
 class LinksController < ApplicationController
-
+  def index
+    @hot_links = Link.hot
+  end
 end
--- app/models/link.rb
+++ app/models/link.rb
@@ -1,3 +1,9 @@
 class Link < ApplicationRecord
-  
+  scope :hot, -> {
+    select('links.url as url')
+      .joins('join reads on reads.link_id = links.id')
+      .where('reads.created_at > ?', Time.now - 1.day)
+      .group("links.url")
+      .order('count("reads".id) DESC').limit(10)
+  }
 end
